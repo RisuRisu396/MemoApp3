4f421465ae52e2d5fefbf2ed6c775149f5a19c6e
RisuRisu396 <RisuRisu396@gmail.com>
2023-07-16 17:42:52 +0900

    create signUpScreen

--- udemy/MemoApp3/App.jsx
+++ udemy/MemoApp3/App.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import MemoListScreen from './src/screens/MemoListScreen';
 import MemoEditScreen from './src/screens/MemoEditScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
+import LogInScreen from './src/screens/LogInScreen';
+import SignUpScreen from './src/screens/SIgnUpScreen';
 
 export default function App() {
   return (
-    <MemoEditScreen />
+    <SignUpScreen />
   );
 }
